cd20d1e5073e270d09e6d1f25a9b4608ecc4d607
Arobase <bokaserge92@gmail.com>
2023-08-11 13:40:42 -0700

    prints the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet lowercase reverse
+ *
+ * Return: 0 (all success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+		putchar(n);
+	putchar(10);
+
+	return (0);
+}
