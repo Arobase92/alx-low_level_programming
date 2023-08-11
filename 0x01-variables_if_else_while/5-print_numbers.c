999ffbbc8a83e3ff5efd56839f8d65bb0a9df342
Arobase <bokaserge92@gmail.com>
2023-08-11 11:13:19 -0700

    Prints digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints digit numbers
+ *
+ * Return: 0 (all success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	putchar(10);
+
+	return (0);
+}
