888726ef222610e324678b3ceb83807f86a2785d
Arobase <bokaserge92@gmail.com>
2023-08-11 11:34:48 -0700

    Prints digit numbers with the function putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints digit numbers
+ *
+ * Return: 0 (all success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	putchar(10);
+
+	return (0);
+}
