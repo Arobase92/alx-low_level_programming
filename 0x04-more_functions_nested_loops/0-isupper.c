27079bf54c28fb992c1dfa2c3aea3ce9cb2bb48d
Arobase <bokaserge92@gmail.com>
2023-08-17 05:36:33 -0700

    Check uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+
+/**
+ * _isupper - check if a character is uppercase
+ * @c: character to check
+ *
+ * Return: 1 (upper) 0 (other)
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry program
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	printf("%d\n", _isupper('A'));
+
+	return (0);
+}
