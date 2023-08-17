a70bb938ab20c732a85b4d2664653c966f7af6a4
Arobase <bokaserge92@gmail.com>
2023-08-16 19:21:20 -0700

    Check if a character is alphabetic

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+
+/**
+ * _isalpha - check if a character is alphabetic character
+ * @c: The character to check
+ *
+ * Return: On succes 1.
+ */
+int _isalpha(int c)
+{
+	if (islapha(c))
+		return (1);
+	else
+		return (0);
+}
