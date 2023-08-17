dc00605dc62ffa90cd54aa6f8a12d34a80355258
Arobase <bokaserge92@gmail.com>
2023-08-16 20:00:37 -0700

    Prints the last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: number to print the last digit
+ *
+ * Return: int
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
