77d8fe3a1d73b4817304c40279b3584b8fab5909
Arobase <bokaserge92@gmail.com>
2023-08-21 17:01:59 -0700

    returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - compute the length of string
+ * @s: the string
+ *
+ * Return: the length to s
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+
+	return (length);
+}
