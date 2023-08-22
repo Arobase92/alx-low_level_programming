9c628254914c0cb518c5823bbb3f2c53b2049434
Arobase <bokaserge92@gmail.com>
2023-08-22 05:28:44 -0700

    Prints character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - print character
+ *
+ * @str: character to print
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+	int length;
+
+	length = strlen(str);
+	for (i = 0; i < length - 1; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+}
