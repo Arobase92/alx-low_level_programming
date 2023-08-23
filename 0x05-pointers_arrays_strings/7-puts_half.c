0d047e59a09728615ac24da4bf6d168f0dee74df
Arobase <bokaserge92@gmail.com>
2023-08-22 18:12:22 -0700

    unction that prints half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,19 +8,17 @@
  */
 void puts_half(char *str)
 {
-	int i, j, max;
+	int length;
+	int start;
+	int i;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	max = i;
-	j = max / 2;
-	while (j <= max)
-	{
-		_putchar(str[j]);
-		j++;
-	}
+	length = strlen(str);
+	if (length % 2 == 0)
+		start = length / 2;
+	else
+		start = (length - 1) / 2;
+
+	for (i = start; i < length; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
