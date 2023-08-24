a4250f90f483a5346aa82ea5981c479ff524a6cf
Arobase <bokaserge92@gmail.com>
2023-08-23 17:14:10 -0700

    function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ *
+ * @s1: string 1
+ * @s2 : string 2
+ *
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
