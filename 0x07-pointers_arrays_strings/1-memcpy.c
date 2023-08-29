7c86b205642049506152c00da4846db4b67fd26f
Arobase <bokaserge92@gmail.com>
2023-08-28 18:52:32 -0700

    function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _memcpy - function that copy address source to address destination
+ *
+ * @dest: pointer to address destination
+ * @src: pointer to address source
+ * @n: the size octet to copy
+ *
+ * Return: s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpt(dest, src, n);
+	return (dest);
+}
