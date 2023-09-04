2315ba044115b0abe902714d29b2a9735a4ea5f3
Arobase <bokaserge92@gmail.com>
2023-09-04 01:29:14 -0700

    creates an array of chars, and initializes it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	i = 0;
 	tab = malloc(sizeof(char) * size);
 
-	if (tab == NULL)
+	if (tab == NULL || size == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		tab[i] = c;
