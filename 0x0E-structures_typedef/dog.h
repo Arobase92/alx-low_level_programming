391a2f26b6819036a0611959274e7ee68f854c89
Arobase <bokaserge92@gmail.com>
2023-09-11 18:01:56 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_F
+#define DOG_F
+
+/**
+ * struct dog - define a new type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
