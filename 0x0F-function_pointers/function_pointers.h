41b46bfdc83cdcafbb0bfbb4fbacd5e56b11ec6e
Arobase <bokaserge92@gmail.com>
2023-09-13 19:59:23 -0700

    function that searches for an integer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
