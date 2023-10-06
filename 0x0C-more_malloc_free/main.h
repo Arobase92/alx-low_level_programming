6a1bd68777d75656a0a187098a025e6c8317305f
Arobase <bokaserge92@gmail.com>
2023-10-05 19:45:00 -0700

    function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 void *malloc_checked(unsigned int b);
 int _strlen(char *string);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
